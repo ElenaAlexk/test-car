6f1dc6244e2a57aa34275cf410dd0d72c64b767b
ElenaAlexk <111981718+ElenaAlexk@users.noreply.github.com>
2023-05-17 16:22:49 +0000

    Deploying to gh-pages from @ ElenaAlexk/goit-react-hw-08-phonebook@a60cce66da14ade2b36fd32820aca914e18afcb6 🚀

---
+++ static/js/266.b8e79a3f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{266:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(329),i={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function c(){return(0,r.jsx)("div",{style:i.container,children:(0,r.jsx)("h1",{style:i.title,children:"Welcome to your Phonebook manager "})})}}}]);
+//# sourceMappingURL=266.b8e79a3f.chunk.js.map
