d9d875c793fb13ecfed65c30baf2f54201c8debb
Elena Kvasko <lenakvasko@gmail.com>
2023-09-05 08:34:43 +0300

    first commit

--- src/redux/contactsSlice/selectors.js
+++
@@ -1,6 +0,0 @@
-export const selectContacts = state => {
-  //console.log('!', state.contacts);
-  return state.contacts.contacts;
-};
-export const selectError = state => state.contacts.error;
-export const selectIsLoading = state => state.contacts.isLoading;
